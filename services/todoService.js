e67e6d885cf980226e7bc1aebab1f1dd019c2c11
danielsahkwadwo <daniel.sah@amalitech.com>
2025-04-04 15:20:55 +0000

    modified app

--- services/todoService.js
+++ services/todoService.js
@@ -2,7 +2,7 @@ const { ddbDocClient } = require("../config/config");
 const { PutCommand, GetCommand, ScanCommand, UpdateCommand, DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 const { v4: uuidv4 } = require("uuid");
 
-const TABLE_NAME = "MyWeek7Table";
+const TABLE_NAME = process.env.TABLE_NAME;
 class Todo {
   //create todo
   async createTodo(todoData) {
@@ -55,44 +55,44 @@ class Todo {
     const expressionParts = [];
     const expressionNames = {};
     const expressionValues = {};
-    
+
     if (todoData.title !== undefined) {
-      expressionParts.push('#t = :title');
-      expressionNames['#t'] = 'title';
-      expressionValues[':title'] = todoData.title;
+      expressionParts.push("#t = :title");
+      expressionNames["#t"] = "title";
+      expressionValues[":title"] = todoData.title;
     }
-    
+
     if (todoData.description !== undefined) {
-      expressionParts.push('#d = :description');
-      expressionNames['#d'] = 'description';
-      expressionValues[':description'] = todoData.description;
+      expressionParts.push("#d = :description");
+      expressionNames["#d"] = "description";
+      expressionValues[":description"] = todoData.description;
     }
-    
+
     if (todoData.priority !== undefined) {
-      expressionParts.push('#p = :priority');
-      expressionNames['#p'] = 'priority';
-      expressionValues[':priority'] = todoData.priority;
+      expressionParts.push("#p = :priority");
+      expressionNames["#p"] = "priority";
+      expressionValues[":priority"] = todoData.priority;
     }
-    
+
     if (todoData.completed !== undefined) {
-      expressionParts.push('#c = :isCompleted');
-      expressionNames['#c'] = 'isCompleted';
-      expressionValues[':isCompleted'] = todoData.isCompleted;
+      expressionParts.push("#c = :isCompleted");
+      expressionNames["#c"] = "isCompleted";
+      expressionValues[":isCompleted"] = todoData.isCompleted;
     }
-    
+
     if (expressionParts.length === 0) {
-      throw new Error('No valid fields provided for update.');
+      throw new Error("No valid fields provided for update.");
     }
-    
+
     const command = new UpdateCommand({
       TableName: TABLE_NAME,
       Key: { id },
-      UpdateExpression: `SET ${expressionParts.join(', ')}`,
+      UpdateExpression: `SET ${expressionParts.join(", ")}`,
       ExpressionAttributeNames: expressionNames,
       ExpressionAttributeValues: expressionValues,
-      ReturnValues: 'ALL_NEW',
+      ReturnValues: "ALL_NEW",
     });
-    
+
     const result = await ddbDocClient.send(command);
     return result.Attributes;
   }
